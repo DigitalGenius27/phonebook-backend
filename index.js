85188d21a04cbae388fe2128fa3a6b5939b6d1d6
Noel Alfonso Obando Martinez <147889470+DigitalGenius27@users.noreply.github.com>
2026-09-12 09:48:38 -0600

    Complete exercise 3.14

--- index.js
+++ index.js
@@ -82,11 +82,9 @@ app.post('/api/persons', (request, response) => {
   }
 
   persons = persons.concat(person)
-
   response.status(201).json(person)
 })
 
-const PORT = process.env.PORT || 3001
 
 connectToDatabase(MONGODB_URI).then(() => {
   app.listen(PORT, () => {
