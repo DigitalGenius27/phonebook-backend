60e6d3dade6c79d91b3d20ccf45701efada96c2e
Noel Alfonso Obando Martinez <147889470+DigitalGenius27@users.noreply.github.com>
2026-09-12 10:00:40 -0600

    Complete exercise 3.19

--- index.js
+++ index.js
@@ -109,6 +109,12 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: 'malformatted id' })
   }
 
+  if (error.name === 'ValidationError') {
+  return response.status(400).json({
+    error: error.message
+  })
+}
+
   next(error)
 }
 
