84ef72aa4358f029fd0ffb7b0e7491d73491f49f
Noel Alfonso Obando Martinez <147889470+DigitalGenius27@users.noreply.github.com>
2026-09-12 08:40:51 -0600

    Add static file serving from 'dist' directory

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const path = require('path')
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'dist')))
 
 morgan.token('body', request => {
   return JSON.stringify(request.body)
