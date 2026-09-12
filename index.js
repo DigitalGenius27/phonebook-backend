1fdc4597f8c4a70d09196d5d9019213b8812425e
Noel Alfonso Obando Martinez <147889470+DigitalGenius27@users.noreply.github.com>
2026-09-12 09:39:24 -0600

    Complete exercise 3.13

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const path = require('path')
+const { MONGODB_URI, PORT } = require('./utils/config')
+const { connectToDatabase } = require('./utils/mongo')
+const Person = require('./models/person')
 
 const app = express()
 
@@ -16,42 +19,23 @@ morgan.token('body', request => {
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 )
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456'
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523'
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345'
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122'
-  }
-]
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/info', (request, response) => {
-  const date = new Date()
+  Person.countDocuments({}).then(count => {
+    const date = new Date()
 
-  response.send(`
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${date}</p>
-  `)
+    response.send(`
+      <p>Phonebook has info for ${count} people</p>
+      <p>${date}</p>
+    `)
+  })
 })
-
 app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
 
@@ -104,6 +88,8 @@ app.post('/api/persons', (request, response) => {
 
 const PORT = process.env.PORT || 3001
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+connectToDatabase(MONGODB_URI).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
 })
